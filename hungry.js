c27747a28a7b723ae29a446c81fb9e5414801bdb
pavan varma <pavan.vadapalli009@gmail.com>
2020-04-09 14:54:45 +0530

    added hungry prompt

--- hungry.js
+++ hungry.js
@@ -1,1 +1,18 @@
-console.log("I'm hungry")
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Are you hungry ', (answer) => {
+  if(answer=="yes")
+  {
+  console.log(`eat samosa`);
+  }
+  else{
+      console.log('do your homework');
+  }
+
+  rl.close();
+});
